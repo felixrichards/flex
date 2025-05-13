0a62fa14f58a38d2ff98301ba201f9ed57850b2d
felixrichards <felixarichards@gmail.com>
2025-05-13 14:21:01 +0100

    Add test script

--- random_champ_bot/test.py
+++ random_champ_bot/test.py
@@ -1,6 +1,20 @@
-from champs.random_champ_weighted import get_random_champs_weighted, make_grid_from_champs
+import sys
+from champs import random_champ_weighted
 
+if __name__ == "__main__":
+    N = 40
+    filter_strs = None
+    if len(sys.argv) > 1:
+        N = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        filter_strs = sys.argv[2:]
 
-champs = get_random_champs_weighted(40)
-img = make_grid_from_champs(champs)
-img.save("test.png", format='PNG')
+    if filter_strs:
+        champs = random_champ_weighted.get_random_champs_with_filters(N=N, filter_strs=filter_strs)
+        img = random_champ_weighted.make_grid_from_champs(champs, force_line=True)
+    else:
+        selected_champs_by_role = random_champ_weighted.get_random_champs_by_role_weighted(N=N)
+        champs = sum(selected_champs_by_role.values(), start=[])
+        img = random_champ_weighted.make_grid_from_champs_by_role(selected_champs_by_role)
+    img.save("test.png")
+    print(", ".join(champs))
