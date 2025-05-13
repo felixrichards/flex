a91a44a1c6dff4a56d074e3f97a939094cdb7a86
felixrichards <felixarichards@gmail.com>
2025-05-13 14:14:53 +0100

    More resources

--- random_champ_bot/champs/myresources/__init__.py
+++ random_champ_bot/champs/myresources/__init__.py
@@ -4,9 +4,51 @@ from PIL import Image
 
 MY_DIR = os.path.dirname(os.path.abspath(__file__))
 
-CHAMPS_WITH_ROLE_DATA_FILENAME = "champs_by_role.txt"
+CHAMPS_WITH_ROLE_DATA_FILENAME = "champs_with_roles.txt"
 with open(os.path.join(MY_DIR, CHAMPS_WITH_ROLE_DATA_FILENAME), "r") as f:
     CHAMPS_WITH_ROLE_DATA = f.read().split("\n")
+
+
+ROLES_BY_CHAMP_FILENAME = "roles_by_champ.txt"
+with open(os.path.join(MY_DIR, ROLES_BY_CHAMP_FILENAME), "r") as f:
+    ROLES_BY_CHAMP = {}
+    for data in f.read().split('\n'):
+        champ, role = data.split("\t")[0], data.split("\t")[1].split(",")
+        ROLES_BY_CHAMP[champ] = role
+
+CHAMPS_BY_ROLE_FILENAME = "champs_by_role.txt"
+with open(os.path.join(MY_DIR, CHAMPS_BY_ROLE_FILENAME), "r") as f:
+    CHAMPS_BY_ROLE = {}
+    for data in f.read().split('\n'):
+        role, champs = data.split("\t")[0], data.split("\t")[1].split(",")
+        CHAMPS_BY_ROLE[role] = champs
+
+CHAMPS_BY_DAMAGE_TYPE_FILENAME = "champs_by_damage_type.txt"
+with open(os.path.join(MY_DIR, CHAMPS_BY_DAMAGE_TYPE_FILENAME), "r") as f:
+    CHAMPS_BY_DAMAGE_TYPE = {}
+    for data in f.read().split('\n'):
+        damage_type, champs = data.split("\t")[0], data.split("\t")[1].split(",")
+        CHAMPS_BY_DAMAGE_TYPE[damage_type] = champs
+
+CHAMPS_FILENAME = "champs.txt"
+with open(os.path.join(MY_DIR, CHAMPS_FILENAME), "r") as f:
+    CHAMPS = f.read().split('\n')
+
+
+CLASSES_WITH_CHAMP_DATA_FILENAME = "champs_by_class.txt"
+with open(os.path.join(MY_DIR, CLASSES_WITH_CHAMP_DATA_FILENAME), "r") as f:
+    CHAMPS_BY_CLASS = {}
+    for data in f.read().split('\n'):
+        league_class, champs = data.split("\t")[0], data.split("\t")[1].split(",")
+        CHAMPS_BY_CLASS[league_class] = champs
+
+
+CHAMPS_BY_LGBT_FILENAME = "champs_by_lgbt.txt"
+with open(os.path.join(MY_DIR, CHAMPS_BY_LGBT_FILENAME), "r") as f:
+    CHAMPS_BY_LGBT = {}
+    for data in f.read().split('\n'):
+        lgbt_type, champs = data.split("\t")[0], data.split("\t")[1].split(",")
+        CHAMPS_BY_LGBT[lgbt_type] = champs
 
 
 CHAMP_IMAGES_FILENAMES = glob.glob(os.path.join(MY_DIR, "images/*"))
