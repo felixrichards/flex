cf47eb966d7aa405620ed5cc9587186a13aea91c
Felix Richards <felixarichards@gmail.com>
2026-04-29 01:47:18 +0100

    Unify champ metadata resources

--- champs/myresources/__init__.py
+++ champs/myresources/__init__.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import json
+import yaml
 from PIL import Image
 
 RESOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -15,68 +16,49 @@ with open(os.path.join(RESOURCE_DIR, PLAYER_TO_NAME_FILENAME), "r") as f:
     PLAYER_TO_NAME = json.load(f)
 
 
-CHAMPS_WITH_ROLE_DATA_FILENAME = "champs_with_roles.txt"
-with open(os.path.join(RESOURCE_DIR, CHAMPS_WITH_ROLE_DATA_FILENAME), "r") as f:
-    CHAMPS_WITH_ROLE_DATA = f.read().split("\n")
+CHAMP_DATA_DB_FILENAME = "champ_data_db.yaml"
+with open(os.path.join(RESOURCE_DIR, CHAMP_DATA_DB_FILENAME), "r") as f:
+    _champ_data_db = yaml.safe_load(f)
+
+_champions = _champ_data_db["champions"]
+_role_order = ["TOP", "JUNGLE", "MID", "BOT", "SUPP"]
+
+CHAMPS_WITH_ROLE_DATA = []
+for champion in _champions:
+    flags = [role if role in champion["roles"] else "" for role in _role_order]
+    CHAMPS_WITH_ROLE_DATA.append("\t".join([champion["name"], *flags, ""]))
+
+ROLES_BY_CHAMP = {champion["name"]: champion["roles"] for champion in _champions}
+
+CHAMPS_BY_ROLE = {}
+for role in _role_order:
+    CHAMPS_BY_ROLE[role] = [champion["name"] for champion in _champions if role in champion["roles"]]
+
+CHAMPS_BY_DAMAGE_TYPE = {}
+for champion in _champions:
+    for damage_type in champion["damage"]:
+        CHAMPS_BY_DAMAGE_TYPE.setdefault(damage_type, []).append(champion["name"])
 
 
-ROLES_BY_CHAMP_FILENAME = "roles_by_champ.txt"
-with open(os.path.join(RESOURCE_DIR, ROLES_BY_CHAMP_FILENAME), "r") as f:
-    ROLES_BY_CHAMP = {}
-    for data in f.read().split('\n'):
-        champ, role = data.split("\t")[0], data.split("\t")[1].split(",")
-        ROLES_BY_CHAMP[champ] = role
+CHAMPS = [champion["name"] for champion in _champions]
 
-CHAMPS_BY_ROLE_FILENAME = "champs_by_role.txt"
-with open(os.path.join(RESOURCE_DIR, CHAMPS_BY_ROLE_FILENAME), "r") as f:
-    CHAMPS_BY_ROLE = {}
-    for data in f.read().split('\n'):
-        role, champs = data.split("\t")[0], data.split("\t")[1].split(",")
-        CHAMPS_BY_ROLE[role] = champs
+CHAMPS_BY_CLASS = {}
+for champion in _champions:
+    for league_class in champion["classes"]:
+        CHAMPS_BY_CLASS.setdefault(league_class, []).append(champion["name"])
 
-CHAMPS_BY_DAMAGE_TYPE_FILENAME = "champs_by_damage_type.txt"
-with open(os.path.join(RESOURCE_DIR, CHAMPS_BY_DAMAGE_TYPE_FILENAME), "r") as f:
-    CHAMPS_BY_DAMAGE_TYPE = {}
-    for data in f.read().split('\n'):
-        damage_type, champs = data.split("\t")[0], data.split("\t")[1].split(",")
-        CHAMPS_BY_DAMAGE_TYPE[damage_type] = champs
+CHAMPS_BY_GENDER = {}
+for champion in _champions:
+    CHAMPS_BY_GENDER.setdefault(champion["gender"], []).append(champion["name"])
 
-CHAMPS_FILENAME = "champs.txt"
-with open(os.path.join(RESOURCE_DIR, CHAMPS_FILENAME), "r") as f:
-    CHAMPS = f.read().split('\n')
+CHAMPS_BY_LGBT = {}
+for champion in _champions:
+    CHAMPS_BY_LGBT.setdefault(champion["lgbt"], []).append(champion["name"])
 
-
-CLASSES_WITH_CHAMP_DATA_FILENAME = "champs_by_class.txt"
-with open(os.path.join(RESOURCE_DIR, CLASSES_WITH_CHAMP_DATA_FILENAME), "r") as f:
-    CHAMPS_BY_CLASS = {}
-    for data in f.read().split('\n'):
-        league_class, champs = data.split("\t")[0], data.split("\t")[1].split(",")
-        CHAMPS_BY_CLASS[league_class] = champs
-
-
-CHAMPS_BY_GENDER_FILENAME = "champs_by_gender.txt"
-with open(os.path.join(RESOURCE_DIR, CHAMPS_BY_GENDER_FILENAME), "r") as f:
-    CHAMPS_BY_GENDER = {}
-    for data in f.read().split('\n'):
-        gender_type, champs = data.split("\t")[0], data.split("\t")[1].split(",")
-        CHAMPS_BY_GENDER[gender_type] = champs
-
-
-CHAMPS_BY_LGBT_FILENAME = "champs_by_lgbt.txt"
-with open(os.path.join(RESOURCE_DIR, CHAMPS_BY_LGBT_FILENAME), "r") as f:
-    CHAMPS_BY_LGBT = {}
-    for data in f.read().split('\n'):
-        lgbt_type, champs = data.split("\t")[0], data.split("\t")[1].split(",")
-        CHAMPS_BY_LGBT[lgbt_type] = champs
-
-
-CHAMPS_BY_COMPLEXION_FILENAME = "champs_by_complexion.txt"
-with open(os.path.join(RESOURCE_DIR, CHAMPS_BY_COMPLEXION_FILENAME), "r") as f:
-    CHAMPS_BY_COMPLEXION = {}
-    for data in f.read().split('\n'):
-        complexion, champs = data.split("\t")[0], data.split("\t")[1].split(",")
-        CHAMPS_BY_COMPLEXION[complexion] = champs
-
+CHAMPS_BY_COMPLEXION = {}
+for champion in _champions:
+    complexion = str(champion["complexion"])
+    CHAMPS_BY_COMPLEXION.setdefault(complexion, []).append(champion["name"])
 
 
 CHAMP_IMAGES_FILENAMES = glob.glob(os.path.join(RESOURCE_DIR, "images/*.png"))
